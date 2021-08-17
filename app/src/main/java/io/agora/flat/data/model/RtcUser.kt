1f805eb5f84eacad96c5d0187d641ddd58b5e48b
fenglibin <fenglibin@agora.io>
2021-08-17 18:39:33 +0800

    fix audio & video state sync error

--- app/src/main/java/io/agora/flat/data/model/RtcUser.kt
+++ app/src/main/java/io/agora/flat/data/model/RtcUser.kt
@@ -1,11 +1,13 @@
 package io.agora.flat.data.model
 
 data class RtcUser(
-    var name: String = "", var rtcUID: Int = NOT_JOIN_RTC_UID, var avatarURL: String = "",
+    var name: String = "",
+    var rtcUID: Int = NOT_JOIN_RTC_UID,
+    var avatarURL: String = "",
 
     var userUUID: String = "",
-    var audioOpen: Boolean = true,
-    var videoOpen: Boolean = true,
+    var audioOpen: Boolean = false,
+    var videoOpen: Boolean = false,
     var isSpeak: Boolean = false,
     var isRaiseHand: Boolean = false,
 ) {
