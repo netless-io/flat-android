0fc0f14514570c0c0cdddd7cf92e6188657f22b8
fenglibin <fenglibin@agora.io>
2022-01-06 12:36:18 +0800

    fix FlatClickableText display in darkmode

--- app/src/main/java/io/agora/flat/ui/compose/FlatClickableText.kt
+++ app/src/main/java/io/agora/flat/ui/compose/FlatClickableText.kt
@@ -1,13 +1,13 @@
 package io.agora.flat.ui.compose
 
 import androidx.compose.foundation.text.ClickableText
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.withStyle
 import io.agora.flat.common.Navigator
-import io.agora.flat.ui.theme.FlatColorBlue
 
 data class ClickableItem(val part: String, val tag: String, val annotation: String)
 
@@ -20,18 +20,22 @@ fun FlatClickableText(text: String, items: List<ClickableItem>) {
         while (last < text.length) {
             val pair = text.findAnyOf(strings = items.map { it.part }, last)
             if (pair != null) {
-                append(text.substring(last, pair.first))
+                withStyle(style = SpanStyle(MaterialTheme.colors.onSurface)) {
+                    append(text.substring(last, pair.first))
+                }
                 items.find { it.part == pair.second }?.run {
                     pushStringAnnotation(tag = tag, annotation = annotation)
                 }
-                withStyle(style = SpanStyle(color = FlatColorBlue)) {
+                withStyle(style = SpanStyle(MaterialTheme.colors.primary)) {
                     append(pair.second)
                 }
                 pop()
 
                 last = pair.first + pair.second.length
             } else {
-                append(text.substring(last))
+                withStyle(style = SpanStyle(MaterialTheme.colors.onSurface)) {
+                    append(text.substring(last))
+                }
                 break
             }
         }
