4e1daae077c2fc96ce8af3fb52552117caa6a8d9
aderan <fenglibinjie@gmail.com>
2022-03-11 13:54:28 +0800

    Bump fastboard-android to 1.0.0-beta.4

--- app/src/main/java/io/agora/flat/common/board/BoardRoom.kt
+++ app/src/main/java/io/agora/flat/common/board/BoardRoom.kt
@@ -2,22 +2,18 @@ package io.agora.flat.common.board
 
 import android.content.Context
 import android.util.Log
-import com.herewhite.sdk.RoomParams
-import com.herewhite.sdk.WhiteSdkConfiguration
 import com.herewhite.sdk.domain.*
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.android.scopes.ActivityRetainedScoped
 import io.agora.board.fast.FastRoom
-import io.agora.board.fast.FastSdk
+import io.agora.board.fast.Fastboard
 import io.agora.board.fast.FastboardView
 import io.agora.board.fast.model.FastRoomOptions
-import io.agora.board.fast.model.FastSdkOptions
 import io.agora.board.fast.ui.RoomControllerGroup
 import io.agora.flat.Constants
 import io.agora.flat.data.AppKVCenter
 import io.agora.flat.data.repository.UserRepository
 import io.agora.flat.di.interfaces.IBoardRoom
-import io.agora.flat.util.isApkInDebug
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -35,7 +31,7 @@ class BoardRoom @Inject constructor(
         const val TAG = "BoardRoom"
     }
 
-    private lateinit var fastSdk: FastSdk
+    private lateinit var fastboard: Fastboard
     private var fastboardView: FastboardView? = null
     private var fastRoom: FastRoom? = null
     private var darkMode: Boolean = false
@@ -46,15 +42,7 @@ class BoardRoom @Inject constructor(
 
     override fun initSdk(fastboardView: FastboardView) {
         this.fastboardView = fastboardView
-
-        val configuration = WhiteSdkConfiguration(Constants.NETLESS_APP_IDENTIFIER).apply {
-            isUserCursor = true
-            useMultiViews = true
-            isLog = context.isApkInDebug()
-        }
-        val options = FastSdkOptions(Constants.NETLESS_APP_IDENTIFIER)
-        options.configuration = configuration
-        fastSdk = fastboardView.getFastSdk(options)
+        this.fastboard = fastboardView.fastboard
     }
 
     override fun setRoomController(rootRoomController: RoomControllerGroup) {
@@ -62,21 +50,10 @@ class BoardRoom @Inject constructor(
     }
 
     override fun join(roomUUID: String, roomToken: String, userId: String, writable: Boolean) {
-        val roomParams = RoomParams(roomUUID, roomToken, userId).apply {
-            val styleMap = hashMapOf(
-                "bottom" to "30px",
-                "right" to "44px",
-                "position" to "fixed",
-            )
-            windowParams = WindowParams()
-                .setChessboard(false)
-                .setDebug(true)
-                .setCollectorStyles(styleMap)
-                .setContainerSizeRatio(9.0f / 16)
+        val fastRoomOptions = FastRoomOptions(Constants.NETLESS_APP_IDENTIFIER, roomUUID, roomToken, userId)
+        val roomParams = fastRoomOptions.roomParams.apply {
             windowParams.setPrefersColorScheme(if (darkMode) WindowPrefersColorScheme.Dark else WindowPrefersColorScheme.Light)
-
             isWritable = writable
-            isDisableNewPencil = false
             userPayload = UserPayload(
                 userId = userRepository.getUserUUID(),
                 nickName = userRepository.getUsername(),
@@ -84,15 +61,13 @@ class BoardRoom @Inject constructor(
             )
             isUseNativeWebSocket = appKVCenter.isNetworkAcceleration()
         }
-        val options = FastRoomOptions(roomUUID, roomToken, userId, writable).apply {
-            this.roomParams = roomParams
-        }
+        fastRoomOptions.roomParams = roomParams
 
-        fastSdk.setErrorHandler { error ->
+        fastboard.setErrorHandler { error ->
             boardRoomPhase.value = BoardRoomPhase.Error(error.message ?: "no error message")
         }
 
-        fastSdk.setRoomPhaseHandler { phase ->
+        fastboard.setRoomPhaseHandler { phase ->
             Log.d(TAG, "onPhaseChanged:${phase.name}")
             when (phase) {
                 RoomPhase.connecting -> boardRoomPhase.value = BoardRoomPhase.Connecting
@@ -101,22 +76,24 @@ class BoardRoom @Inject constructor(
             }
         }
 
-        fastRoom = fastSdk.joinRoom(options)
+        fastboard.joinRoom(fastRoomOptions) {
+            fastRoom = it
+        }
     }
 
     override fun setDarkMode(dark: Boolean) {
         this.darkMode = dark
 
-        if (::fastSdk.isInitialized) {
-            val fastStyle = fastSdk.fastStyle.apply {
+        if (::fastboard.isInitialized) {
+            val fastStyle = fastboard.fastStyle.apply {
                 isDarkMode = dark
             }
-            fastSdk.fastStyle = fastStyle
+            fastboard.fastStyle = fastStyle
         }
     }
 
     override fun release() {
-        fastSdk.destroy()
+        fastboard.destroy()
     }
 
     override fun setWritable(writable: Boolean) {
@@ -128,7 +105,7 @@ class BoardRoom @Inject constructor(
     }
 
     override fun hideAllOverlay() {
-        fastSdk.overlayManger.hideAll()
+        fastboard.overlayManger.hideAll()
     }
 
     override fun insertImage(imageUrl: String, w: Int, h: Int) {
