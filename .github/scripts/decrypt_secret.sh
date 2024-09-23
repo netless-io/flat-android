6d21ae8bb3d107ce0d9105e62670bc721001fdda
aderan <fenglibinjie@gmail.com>
2024-09-23 11:07:21 +0800

    remove google-services.json and update .gitignore to prevent future commits

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -7,6 +7,9 @@ gpg --quiet --batch --yes --decrypt --passphrase="$KEYSTORE_SECRET_PASSPHRASE" \
 # decrpyt gradle properties
 gpg --quiet --batch --yes --decrypt --passphrase="$GRADLE_SECRET_PASSPHRASE" \
 --output $HOME/flat/ci-gradle.properties $GITHUB_WORKSPACE/.github/ci-gradle.properties.gpg
+# decrpyt google-services.json
+gpg --quiet --batch --yes --decrypt --passphrase="$GRADLE_SECRET_PASSPHRASE" \
+--output $GITHUB_WORKSPACE/app/google-services.json $GITHUB_WORKSPACE/.github/google-services.json.gpg
 
 mkdir -p ~/.gradle
 cp $HOME/flat/ci-gradle.properties ~/.gradle/gradle.properties
